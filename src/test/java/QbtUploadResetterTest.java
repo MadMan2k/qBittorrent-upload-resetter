58aba0c45b791917e3d7e463868a708625a819f3
MadMan <mr.mad2k@gmail.com>
2024-06-07 13:31:17 +0200

    add dummy test

---
+++ src/test/java/QbtUploadResetterTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class QbtUploadResetterTest {
+    @Test
+    public void dummyTest() {
+        int expected = 2;
+        int result = 2;
+        assertEquals(expected, result);
+    }
+}
